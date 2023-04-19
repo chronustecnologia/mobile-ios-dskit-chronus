862898fe7ec8c1067f676e90c8ca2152ee5fdaa3
Jose Julio <jose.julio.ext@panonline.com.br>
2023-04-19 12:05:25 -0300

    fix example project

--- Example/ChronusDS/ViewController.swift
+++ Example/ChronusDS/ViewController.swift
@@ -7,18 +7,19 @@
 //
 
 import UIKit
+import ChronusDS
 
 class ViewController: UIViewController {
 
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        self.view = view
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
